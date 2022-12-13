fe1be81c3310af8b235ca202fe1fa28af54727da
ShahariarRahman <shahariarrahman98@gmail.com>
2022-12-13 13:56:51 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    fetch('http://localhost:5000/product')
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="">
+        Hello world!
+      </h1>
     </div>
   );
 }
